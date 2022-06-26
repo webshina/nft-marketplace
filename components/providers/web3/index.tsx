763cb7f5614c5c596f48b068223280dae7aa9bb2
KazukiYamashina <kazu4101719@gmail.com>
2022-06-26 15:57:50 +0900

    セクション6

--- components/providers/web3/index.tsx
+++ components/providers/web3/index.tsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect } from "react";
 import { useState } from "react";
 import { createContext, FunctionComponent } from "react";
-import { createDefaultState, Web3State } from "./utils";
+import { createDefaultState, loadContract, Web3State } from "./utils";
 import { ethers } from "ethers";
 
 const Web3Context = createContext<Web3State>(createDefaultState());
@@ -10,15 +10,16 @@ const Web3Provider: FunctionComponent = ({ children }) => {
   const [web3Api, setWeb3Api] = useState<Web3State>(createDefaultState());
 
   useEffect(() => {
-    function initWeb3() {
+    async function initWeb3() {
       const provider = new ethers.providers.Web3Provider(
         window.ethereum as any
       );
+      const contract = await loadContract("NftMarket", provider);
 
       setWeb3Api({
         ethereum: window.ethereum,
         provider,
-        contract: null,
+        contract: contract,
         isLoading: true,
       });
     }
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,8 +7,16 @@ import { NftMeta } from "@_types/nft";
 import { useWeb3 } from "@providers/web3";
 
 const Home: NextPage = () => {
-  const { provider } = useWeb3();
-  console.log(provider);
+  const { provider, contract } = useWeb3();
+
+  const getNftInfo = async () => {
+    console.log(await contract!.name());
+    console.log(await contract!.symbol());
+  };
+
+  if (contract) {
+    getNftInfo();
+  }
 
   const getAccounts = async () => {
     const accounts = await provider!.listAccounts();
