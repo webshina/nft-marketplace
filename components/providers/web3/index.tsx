568aeed34604ce82608057510e117c83a6f94570
KazukiYamashina <kazu4101719@gmail.com>
2022-06-26 10:16:02 +0900

    セクション5

---
+++ components/providers/web3/index.tsx
@@ -0,0 +1,37 @@
+import { useContext, useEffect } from "react";
+import { useState } from "react";
+import { createContext, FunctionComponent } from "react";
+import { createDefaultState, Web3State } from "./utils";
+import { ethers } from "ethers";
+
+const Web3Context = createContext<Web3State>(createDefaultState());
+
+const Web3Provider: FunctionComponent = ({ children }) => {
+  const [web3Api, setWeb3Api] = useState<Web3State>(createDefaultState());
+
+  useEffect(() => {
+    function initWeb3() {
+      const provider = new ethers.providers.Web3Provider(
+        window.ethereum as any
+      );
+
+      setWeb3Api({
+        ethereum: window.ethereum,
+        provider,
+        contract: null,
+        isLoading: true,
+      });
+    }
+    initWeb3();
+  }, []);
+
+  return (
+    <Web3Context.Provider value={web3Api}>{children}</Web3Context.Provider>
+  );
+};
+
+export function useWeb3() {
+  return useContext(Web3Context);
+}
+
+export default Web3Provider;
