fa9c1243fb4bf5bbad47d807fc297d0d7ebf22dc
KazukiYamashina <kazu4101719@gmail.com>
2022-09-12 16:15:16 +0900

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,17 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { Navbar } from "../components/ui";
-import { Web3Provider } from "@providers";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { Web3Provider } from '@providers';
+import type { AppProps } from 'next/app';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <ToastContainer />
       <Web3Provider>
-        <Component {...pageProps} />
+        <>
+          <Component {...pageProps} />
+        </>
       </Web3Provider>
     </>
   );
