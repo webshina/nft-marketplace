7942115d6b0134c6b5311170c8b87f674feb3d8c
KazukiYamashina <kazu4101719@gmail.com>
2022-09-12 16:30:09 +0900

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,14 @@
 import { Web3Provider } from '@providers';
-import type { AppProps } from 'next/app';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }) {
   return (
     <>
       <ToastContainer />
       <Web3Provider>
-        <>
-          <Component {...pageProps} />
-        </>
+        <Component {...pageProps} />
       </Web3Provider>
     </>
   );
